b7f733b65590ca8a792cbb309335607e7398fa71
stevecallender <steven.nm.callender@gmail.com>
2020-01-28 18:45:58 +0000

    Fixed build error

--- TripLog/TableViewExample.xaml.cs
+++ TripLog/TableViewExample.xaml.cs
@@ -66,7 +66,7 @@ namespace TripLog
         {
             viewModel.OnDeleteClicked(sender, e);
 
-            var noteRemoved = (Note)BindingContext;
+            /*var  noteRemoved = (Note)BindingContext;
             await App.Database.DeleteNoteAsync(noteRemoved);
 
             List<Note> notesList = await App.Database.GetNotesAsync();
@@ -78,7 +78,7 @@ namespace TripLog
             }
 
             Notes = temp;
-
+            */
         }
         /*
         void VisibleFeatures_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
