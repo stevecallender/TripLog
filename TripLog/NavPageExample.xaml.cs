52fe6ff6db8340d020d3719724049aa3e09e57b3
stevecallender <steven.nm.callender@gmail.com>
2020-02-21 17:21:32 +0000

    Added nav page example

---
+++ TripLog/NavPageExample.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TripLog
+{
+    public partial class NavPageExample : TabbedPage
+    {
+        public NavPageExample()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
